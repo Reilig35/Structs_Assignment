b82c37525ff7ca4c3590738e9a1ccb7fcbdca721
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-24 20:09:02 +0100

    Add project files.

---
+++ Structs_Assignment/Number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs_Assignment
+{
+    //Using a struct instead of a class
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Structs_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Structs_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number cash = new Number
+            {
+                Amount = 3580
+            };
+
+            Console.WriteLine(cash.Amount);
+        }
+    }
+}
